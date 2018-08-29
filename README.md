b0432cbef8dd3146697ce1ad89265466fc6b6a50
SunyiNyufi <sunyinyufi@gmail.com>
2018-08-29 23:51:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# GG-Discord-Dating-Sim
---
+++ main.py
@@ -0,0 +1,8 @@
+print("Will you date me?")
+answer = input()
+if answer == "yes":
+    print("Yay!")
+    print("And they lived happily ever after")
+else:
+    print(":(")
+    print("#foreveralone")
